a19048ffdecad7d8a0980619dcf1d5a6c949273e
Soseh <sosehv@gmail.com>
2025-01-10 13:11:16 -0500

    Adds comments, adds show specific url by id code

--- express_server.js
+++ express_server.js
@@ -1,32 +1,44 @@
 const express = require("express");
-const app = express();
 const morgan = require('morgan');
+const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.use(morgan('dev')); // Middleware- Enable server side logging
+app.set("view engine", "ejs"); // Set view engine
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+// Home route
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Route for returning URL database in JSON format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Route to render all URLs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("hello", (req, res) => {
+// Simple hello world route
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// Show specific URL by ID
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
