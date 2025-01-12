cdad60d6b6fb4bbeabfe5f7583f1c77cb0a3efff
Soseh <sosehv@gmail.com>
2025-01-11 21:31:38 -0500

    Adds reading from and writing to a file to save urlDatabase

--- express_server.js
+++ express_server.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = 8080; // default port 8080
+const dbFile = path.join(_dirname, 'urls.json');
 
 app.use(morgan('dev')); // Middleware- Enable server side logging
 app.use(express.urlencoded({extended: true }));
 app.set("view engine", "ejs"); // Set view engine
 
-const urlDatabase = {
+/* const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+}; */
+
+let urlDatabase = {};
+
+// Load URL data from the file on server startup
+fs.readFile(dbFile, (err, data) => {
+  if (err) {
+    console.log("Could not read database file:", err);
+  } else {
+    urlDatabase = JSON.parse(data);
+  }
+});
+
+// Save URL data to the file whenever it's updated
+const saveDatabase = () => {
+  fs.writeFile(dbFile, JSON.stringify(urlDatabase, null, 2), (err) => {
+    if (err) {
+      console.log("Error saving database:", err);
+    }
+  });
 };
 
 // Simulates generating a unique short URL id
@@ -55,19 +78,28 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL; // get the long URL from the form data
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL; // Save the new shortURL -> longURL pair in the database
+  saveDatabase(); //Save data to file after each update
   res.redirect(`/urls/${shortURL}`); // Redirect to the newly created URL page
 });
 
 // Show specific URL by ID
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+  if (!longURL) {
+    return res.status(404).send('Short URL not found!');
+  };
+  const templateVars = { id: shortURL, longURL };
   res.render("urls_show", templateVars);
 });
 
 //Redirect any request to "/u/id" to its longURL
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.id];
+  if (!longURL) {
+    return res.status(404).send('Short URL not found!');
+  };
+  res.redirect(301, longURL); // Use 301 for permanent redirection
 });
 
 // Start the server
