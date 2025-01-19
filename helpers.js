8fe5e72b81fe411c0d6b6a7f3fad185355cf909a
Soseh <sosehv@gmail.com>
2025-01-18 23:55:45 -0500

    Initial commit- Adds getUserByEmail function

---
+++ helpers.js
@@ -0,0 +1,11 @@
+// Helper function to lookup user in users object
+const getUserByEmail = function(email, database) {
+  for (const userid in database) {
+    if (database[userid].email === email) {
+      return database[userid];
+    }
+  }
+  return null;
+};
+
+module.exports = getUserByEmail;
