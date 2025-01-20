f4015049d1962adaf0121f70fe763a35a599275c
Soseh <sosehv@gmail.com>
2025-01-20 13:23:10 -0500

    Adds urlsForUser helper function to the file and exports it

--- helpers.js
+++ helpers.js
@@ -8,4 +8,22 @@ const getUserByEmail = function(email, database) {
   return null;
 };
 
-module.exports = { getUserByEmail };
+// Helper function to finding URLs associated with a specific userid
+const urlsForUser = function(userId, urlDatabase) {
+  const userUrls = {};
+
+  // Ensure urlDatabase is not empty or null
+  if (!urlDatabase) {
+    return userUrls;
+  }
+  // Loop through the urlDatabase and add matching URLS to the result object
+  for (const shortURL in urlDatabase) {
+    const urlData = urlDatabase[shortURL];
+    if ( urlData.userId === userId) {
+      userUrls[shortURL] = urlData;
+    }
+  }
+  return userUrls;
+}
+
+module.exports = { getUserByEmail, urlsForUser };
